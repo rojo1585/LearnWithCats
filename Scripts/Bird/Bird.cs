69475b92ec9de913e947998e1e91cc5e16a3b0ef
Rojo1585 <jorge_mendoza1585@hotmail.com>
2022-09-10 23:28:59 -0500

    Eagle Prefab

--- Scripts/Bird/Bird.cs
+++ Scripts/Bird/Bird.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    Rigidbody2D rb2D;
+    Animator animator;
+    [SerializeField] private BoxCollider2D birCollider;
     [SerializeField] private float speed;
+    private bool dead;
+    [SerializeField]private bool onGround;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,13 +24,35 @@ public class Bird : MonoBehaviour
     }
     
     private void MovementBird(){
-        transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        //transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+        if (dead)
+        {
+            DeadBird();
+        }else{
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        
+    }
+
+    private void DeadBird(){
+        animator.SetBool("Dead", true);
+        rb2D.gravityScale = 1;
+        if (onGround)
+        {
+            birCollider.enabled = true;
+            
+        }else{transform.Translate(Vector3.left * speed * Time.deltaTime);}
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision ){
         if(collision.CompareTag("Player")){
+            dead = true;
             PlayerController.Instance.TakeDamagePlayer();
+        }
+        if (collision.CompareTag("Ground"))
+        {
+            onGround = true;
         }
     }
 }
